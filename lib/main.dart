d8023476733ca7ad91237d7535f78d022e9aa32c
KING-CYBERTON <stevekim724@gmail.com>
2023-01-16 00:14:57 -0800

    get

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'GetX Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
